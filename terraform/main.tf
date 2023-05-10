25810b868ac6a653f2deff1e616b344c2e0f8cac
Ahmed abdmouleh <108586834+ahmedabdmouleh8@users.noreply.github.com>
2023-05-10 17:01:10 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,3 @@
-resource "aws_key_pair" "sshkey" {
-  key_name   = "${var.stack}-key"
-  public_key = "${file("${var.ssh_key}")}"
-}
 
 resource "aws_instance" "ec2" {
   ami           = "${data.aws_ami.packer_image.id}"
@@ -9,12 +5,27 @@ resource "aws_instance" "ec2" {
   subnet_id = "${aws_subnet.subnet[0].id}"
   associate_public_ip_address = true
   vpc_security_group_ids = ["${aws_security_group.sg.id}"]
-  key_name = "${aws_key_pair.sshkey.key_name}"
+  key_name   = var.generated_key_name
   tags = {
     Name = "${var.stack}-growlerfriday"
   }
 }
 
+resource "tls_private_key" "dev_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.generated_key_name
+  public_key = tls_private_key.dev_key.public_key_openssh
+
+  provisioner "local-exec" {    # Generate "terraform-key-pair.pem" in current directory
+    command = <<-EOT
+      echo '${tls_private_key.dev_key.private_key_pem}' > ./'${var.generated_key_name}'.pem
+      chmod 400 ./'${var.generated_key_name}'.pem
+    EOT
+  }
 
 data "aws_ami" "packer_image" {
   most_recent = true
