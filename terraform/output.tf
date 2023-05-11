4c2fe8e9d0e7b4d78144e66bd0460c289534a557
Ahmed abdmouleh <108586834+ahmedabdmouleh8@users.noreply.github.com>
2023-05-11 01:04:34 +0100

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -9,3 +9,8 @@ output "curl" {
 output "Login-with-Key" {
     value = "ssh -i ${aws_key_pair.generated_key.key_name}.pem ubuntu@${aws_instance.ec2.public_ip}"
 }
+
+output "generated-key-name" {
+    value = "${var.generated_key_name}.pem"
+}
+
