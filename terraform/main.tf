a91e8d493910efa7e58e3c79146a539dd377e3e7
Ahmed abdmouleh <108586834+ahmedabdmouleh8@users.noreply.github.com>
2023-05-11 01:20:22 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ resource "tls_private_key" "dev_key" {
 }
 
 resource "aws_key_pair" "generated_key" {
-  key_name   = var.generated_key_name
+  key_name   = var.generated_key_name-${timestamp()
   public_key = tls_private_key.dev_key.public_key_openssh
 
   provisioner "local-exec" {    # Generate "terraform-key-pair.pem" in current directory
