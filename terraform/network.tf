96c9e7e92df0c9b03df4ce58c12ac02f2a9e8daf
Ahmed abdmouleh <108586834+ahmedabdmouleh8@users.noreply.github.com>
2023-06-13 02:51:15 +0100

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -69,3 +69,21 @@ resource "aws_security_group_rule" "rule" {
     security_group_id = "${aws_security_group.sg.id}"
     description = "rule-${count.index} port-${var.port_number[count.index]}"
 }
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.sg.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+resource "aws_security_group_rule" "icmp_inbound_rule" {
+  type              = "ingress"
+  from_port         = -1
+  to_port           = -1
+  protocol          = "icmp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg.id
+  description       = "Allow inbound ICMP traffic"
+}
